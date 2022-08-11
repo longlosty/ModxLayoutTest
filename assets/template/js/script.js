7e796d1058030727a06e51249d30cb237021cb9c
Sergei <kinechi1721@gmail.com>
2022-08-11 18:53:00 +0400

    have made a slider for bath houses section and have adapted it

--- assets/template/js/script.js
+++ assets/template/js/script.js
@@ -26,3 +26,129 @@ window.addEventListener("scroll", function() {
 
 bookingBtn.style = "margin:" + introH;
 
+/* Section: Bath-houses */
+new Swiper("#sb1 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 15,
+    pagination: {
+      el: "#sb1 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb2 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb2 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb3 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb3 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb4 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb4 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb5 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb5 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb6 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb6 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb7 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb7 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+new Swiper("#sb8 .swiper", {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: "#sb8 .swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    }
+});
+
+
+    // Set slider only for mobile
+// const bathContent = document.querySelector('.bath-content');
+// let bathContentH = bathContent.clientHeight;
+
+// if (bathContentH > 1500) {
+//     let sliderBh = document.querySelectorAll('#slider-bathHouses');
+//     for (let elem of sliderBh) {
+//         elem.classList.add('slider');
+//         console.log(elem.length);
+//     }
+// }
+
+/* Section: Services */
+new Swiper("#test .swiper", {
+    slidesPerView: 2,
+
+    breakpoints: {
+        839: {
+            slidesPerView: 3, 
+            spaceBetween: 15,
+        },
+
+        1259: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+
+        1689: {
+            slidesPerVIew: 5,
+            spaceBetween: 15,
+        },
+    },
+    
+    spaceBetween: 15000,
+    // loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+
+
